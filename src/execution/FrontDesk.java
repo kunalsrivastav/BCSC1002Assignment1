8217c646f41f0eeb7cac4c44b19077a4e07f7803
Kunal Srivastav <sri2001kunal@gmail.com>
2020-08-29 15:17:52 +0530

    add some printing stuff to FrontDesk.java

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -19,7 +19,12 @@ public class FrontDesk {
         Library library = new Library();
         library.setBooksInLibrary();
         do {
-
+            System.out.println("-=-=--=-=-\"Welcome To The Front Desk\"-=-=--=-=-");
+            System.out.println("How may I help you today?");
+            System.out.println("1. Issue a new book for me.");
+            System.out.println("2. Return a previously issues book for me.");
+            System.out.println("3. Show me all my issues books.");
+            System.out.println("4. Exit.");
         } while (false);
     }
 }
