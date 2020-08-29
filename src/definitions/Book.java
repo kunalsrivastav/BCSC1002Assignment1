4f4066e06e3310cdcc64b6ba74d1295e68a9d666
Kunal Srivastav <sri2001kunal@gmail.com>
2020-08-29 14:35:09 +0530

    add toString() to Book.java

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -48,4 +48,11 @@ public class Book {
     public void setThirteenDigitISBNNumberOfTheBook(String thirteenDigitISBNNumberOfTheBook) {
         this.thirteenDigitISBNNumberOfTheBook = thirteenDigitISBNNumberOfTheBook;
     }
+
+    @Override
+    public String toString() {
+        return "Book Name: " + getNameOfTheBook() + ", " +
+                "Author Name: " + getNameOfTheAuthorOfTheBook() + ", " +
+                "13-Digit ISBN Number: " + getThirteenDigitISBNNumberOfTheBook() + ".";
+    }
 }
